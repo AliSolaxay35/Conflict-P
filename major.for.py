877f00986ba82c8e1d69303283ca3284fe293fb4
AliSolaxay <alibavaria1860@gmail.com>
2024-09-03 22:52:37 +0330

    add major.for.py file

---
+++ major.for.py
@@ -0,0 +1,9 @@
+major = {'immunology', 'hematology', 'microbiology'}
+
+count = 200
+for education in major:    
+    print('salam student')
+    count = count + 100 
+    
+    print('salam students')
+    
